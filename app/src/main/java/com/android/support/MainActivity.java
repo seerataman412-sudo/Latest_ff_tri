bbbc4f7fbb87c05f530c034b1b021f2acf7c53cc
seerataman412-sudo <seerataman412@gmail.com>
2026-09-23 20:33:59 +0530

    Implement unlock and generate key button functionality
    Added functionality for button clicks to unlock panel and generate key.

--- app/src/main/java/com/android/support/MainActivity.java
+++ app/src/main/java/com/android/support/MainActivity.java
@@ -3,45 +3,47 @@ package com.android.support;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    //Only if you have changed MainActivity to yours and you wanna call game's activity.
-    public String GameActivity = "com.unity3d.player.UnityPlayerActivity";
-    public boolean hasLaunched = false;
-
-    //To call onCreate, please refer to README.md
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*Thread.setDefaultUncaughtExceptionHandler(
-                new Thread.UncaughtExceptionHandler() {
-                    @Override
-                    public void uncaughtException(Thread thread, Throwable e) {
-                        Log.e("AppCrash", "Error just lunched ");
-                    }
-                });*/
+        
+        // हमारे XML लेआउट को स्क्रीन पर दिखाना
+        setContentView(getResources().getIdentifier("activity_main", "layout", getPackageName()));
 
+        // XML के बटन्स और इनपुट बॉक्स को जावा से जोड़ना
+        final EditText edtKey = findViewById(getResources().getIdentifier("edtKey", "id", getPackageName()));
+        Button btnUnlock = findViewById(getResources().getIdentifier("btnUnlock", "id", getPackageName()));
+        Button btnGenerate = findViewById(getResources().getIdentifier("btnGenerate", "id", getPackageName()));
 
-        //To launch game activity
-        if (!hasLaunched) {
-            try {
-                //Start service
-                hasLaunched = true;
-                //Launch mod menu.
-                MainActivity.this.startActivity(new Intent(MainActivity.this, Class.forName(MainActivity.this.GameActivity)));
-                Main.Start(this);
-                return;
-            } catch (ClassNotFoundException e) {
-                Log.e("Mod_menu", "Error. Game's main activity does not exist");
-                //Uncomment this if you are following METHOD 2 to launch menu
-                //Toast.makeText(MainActivity.this, "Error. Game's main activity does not exist", Toast.LENGTH_LONG).show();
+        // 1. UNLOCK PANEL बटन पर क्लिक करने का काम
+        btnUnlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputKey = edtKey.getText().toString().trim();
+                
+                // अगर इनपुट खाली नहीं है, तो फ्लोटिंग मेनू चालू करें
+                if (!inputKey.isEmpty()) {
+                    Main.Start(MainActivity.this);
+                    Toast.makeText(MainActivity.this, "Panel Unlocked Successfully!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Please Enter Activation Key", Toast.LENGTH_SHORT).show();
+                }
             }
-        }
+        });
 
-        //Launch mod menu.
-       // Main.StartWithoutPermission(this);
-        Main.Start(this);
+        // 2. GENERATE KEY बटन पर क्लिक करने का काम (सिर्फ मैसेज दिखेगा)
+        btnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Key Generation Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
